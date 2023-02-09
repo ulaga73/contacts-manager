78f2eab5b82ea5bcacfe85d89fa69905a6c6019a
Vinay <niovinay@gmail.com>
2023-02-09 18:39:43 +0530

    added search functionality

--- contacts-manager-frontend/src/components/Search.js
+++ contacts-manager-frontend/src/components/Search.js
@@ -42,7 +42,23 @@ const Search = (props) => {
           <li key={contact.email} onClick={() => onRecommendationSelected(contact)}>{contact.email}</li>
         ))}
       </ul>
-      
+      {/* <div className='container justify-content-between my-2'>
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <a className="navbar-brand disabled" href="#">Total contacts</a>
+                        <button className="navbar-toggler" type="button" >
+                        </button>
+
+                        <div>
+                            <form className="d-flex" role="search">
+                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"
+                                    onChange={handleSearch} />
+                                <button className="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
+            </div> */}
     </div>
   )
 }
--- contacts-manager-frontend/src/components/import/ImportFile.js
+++ contacts-manager-frontend/src/components/import/ImportFile.js
@@ -2,49 +2,50 @@ import "./importFile.css";
 import { useEffect, useState } from "react";
 
 const ImportFile = () => {
-    const [file, setFile] = useState(null);
-    const [apiData, setApiData] = useState(null);
-    console.log(file);
-    useEffect(() => {
-        if(file){
-            const formData = new FormData();
-            formData.append("file", file);
-            fetch("http://localhost:5000/api/contacts", {
-                method: "POST",
-                headers: {
-                    "auth-token": localStorage.getItem('token')
-                },
-                body: formData
-            }).then((res) => {
-                return res.json();
-            }).then((data) => {
-                console.log(data);
-                setApiData(data);
-            })
-        }
-    }, [file])
+  const [file, setFile] = useState(null);
+  const [apiData, setApiData] = useState(null);
+  console.log(file);
+  useEffect(() => {
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      fetch("http://localhost:5000/api/contacts", {
+        method: "POST",
+        headers: {
+          "auth-token": localStorage.getItem('token')
+        },
+        body: formData
+      }).then((res) => {
+        return res.json();
+      }).then((data) => {
+        console.log(data);
+        setApiData(data);
+      })
+    }
+  }, [file])
 
-    return(
-      <div>
-        <div className="import-container">
-            
-            <div className="drag-zone">
-                <p>Drag & Drop a CSV File to Upload</p>
-                <div className="drop-zone">
-                    <input type="file" onChange={(e) => {setFile(e.target.files[0])}} />
-                </div>
-            </div>
-            <div className="import-done container">
-                {apiData && 
-                <>
-                <h2>Import Complete</h2>
-                <p>CSV File is Uploaded</p>
-                </>}
-            </div>
+  return (
+    <div>
+      <div className="import-container">
+
+        <div className="drag-zone">
+          <p>Drag & Drop a CSV File to Upload</p>
+          <div className="drop-zone">
+            <input type="file" onChange={(e) => { setFile(e.target.files[0]) }} />
+          </div>
         </div>
+        <div className="import-done container">
+          {apiData &&
+            <>
+              <h2>Import Complete</h2>
+              <p>CSV File is Uploaded</p>
+            </>}
+        </div>
+      </div>
       <div className="cancel-button">
         <button>Cancel</button>
       </div>
+      
 
       <div className="import-done">
         {apiData && (
